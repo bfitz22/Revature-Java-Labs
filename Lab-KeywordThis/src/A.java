3a41e9f7c9fe39f3832db26c8a4a332723f29dba
bfitz22 <bffitzgerald89@gmail.com>
2019-08-01 23:58:13 -0400

    complete additional java labs

---
+++ Lab-KeywordThis/src/A.java
@@ -0,0 +1,21 @@
+
+public class A {
+	private long id;
+	
+	public A() {
+		this(2L); 
+	}
+	
+	public A(long id) {
+		initialize();
+		this.id = id;
+	}
+	
+	public long getId() { return this.id; }
+	
+	public void setId(long id) { this.id = id; }
+	
+	private void initialize() {
+		System.out.println("initializing the class...");
+	}
+}
