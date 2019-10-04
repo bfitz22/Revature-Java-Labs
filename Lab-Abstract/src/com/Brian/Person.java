65915cc7428846e8eabae4283264ef09843032b6
bfitz22 <bffitzgerald89@gmail.com>
2019-10-03 23:48:41 -0400

    delete unneccessary files

--- Lab-Abstract/src/com/Brian/Person.java
+++
@@ -1,9 +0,0 @@
-package com.Brian;
-
-public abstract class Person {
-	protected String name;
-	
-	public abstract String getName();
-	
-	public abstract void setName(String name);
-}
--- Lab-Abstract/src/com/Brian/test/TestPerson.java
+++
@@ -1,9 +0,0 @@
-package com.Brian.test;
-
-import com.Brian.Person
-
-public class TestPerson {
-	public static void main(String[] args) {
-		Person adam = new Person();
-	}
-}
