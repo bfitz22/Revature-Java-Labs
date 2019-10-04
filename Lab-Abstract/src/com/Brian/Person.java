a0a0167a571155cfd19f8b5f74799eb1ac7d88c0
bfitz22 <bffitzgerald89@gmail.com>
2019-10-03 23:49:25 -0400

    restore Abstract practice file

---
+++ Lab-Abstract/src/com/Brian/Person.java
@@ -0,0 +1,9 @@
+package com.Brian;
+
+public abstract class Person {
+	protected String name;
+	
+	public abstract String getName();
+	
+	public abstract void setName(String name);
+}
---
+++ Lab-Abstract/src/com/Brian/test/TestPerson.java
@@ -0,0 +1,9 @@
+package com.Brian.test;
+
+import com.Brian.Person
+
+public class TestPerson {
+	public static void main(String[] args) {
+		Person adam = new Person();
+	}
+}
